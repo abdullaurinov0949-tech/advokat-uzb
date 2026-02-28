5fa8e7d4702bcfbbab2eea160636b9968d04bb26
Abdulla Urinov <abdullaorinov@gmail.com>
2026-02-28 07:09:51 +0500

    duplicate app removed

--- server.py
+++ server.py
@@ -35,10 +35,5 @@ def send():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
